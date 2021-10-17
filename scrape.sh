1d5eb681ac8f3d1edcc3a650be1d3333c2a88c5f
Patrick Fay <pfay.us@gmail.com>
2021-10-17 13:10:26 -0700

    Updating scrape for new csv locations on new website - removing old names of files that have had their names changed
    see next commit(s) for newly scraped files with new names

---
+++ scrape.sh
@@ -0,0 +1,13 @@
+mkdir -p holdings
+cd holdings
+
+tickers=("ARKK" "ARKQ" "ARKW" "ARKG" "ARKF" "PRNT" "IZRL" "ARKX")
+
+files=()
+for ticker in ${tickers[@]}; do
+  files+=(ARK_INNOVATION_ETF_${ticker}_HOLDINGS.csv)
+done
+
+for file in ${files[@]}; do
+  curl -A "ark-scraper: 6ithub.com/tigger0jk/ark-invest-scraper" https://ark-funds.com/wp-content/uploads/funds-etf-csv/$file | python3 lintCsv.py > $file
+done
